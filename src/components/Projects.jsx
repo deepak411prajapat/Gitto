e73b7718cf7f117038b6bfa160dfa878996f16b4
deepak411prajapat <prajapatdeepak160499@gmail.com>
2025-08-14 18:46:32 +0530

    Added header-feature

---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>My Projects</h2>
+      <ul>
+        <li>Portfolio Website (React JS)</li>
+        <li>Mom's Diary (MERN)</li>
+        <li>E-commerce Website (MERN)</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Projects;
